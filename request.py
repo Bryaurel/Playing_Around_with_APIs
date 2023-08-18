5b3bd6c428fa5291d20ff239531f503586340935
Bryaurel <b.bakongobw@alustudent.com>
2023-08-18 18:26:03 +0000

    request.py

---
+++ request.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import request
+
+url = "https://genius-song-lyrics1.p.rapidapi.com/search/"
+
+querystring = {"q":"<REQUIRED>","per_page":"10","page":"1"}
+
+headers = {
+	"X-RapidAPI-Key": "5d70b22ceemshd6f5559bbad3234p1be2e0jsn556e0e486a85",
+	"X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
